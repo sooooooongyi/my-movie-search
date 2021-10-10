f51e6ad56572cbfe463931670afeae98380c4aba
thddlmy <thddlmy@gmail.com>
2021-10-10 21:34:23 +0900

    feat: import setting change

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import store from '~/store'
 import router from '~/routes'
 import 'bootswatch/dist/litera/bootstrap.min.css'
 
-const app = Vue.createApp(App)
+const { createApp } = Vue
+const app = createApp(App)
 
 app.use(store)
 app.use(router)
