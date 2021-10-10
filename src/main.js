739d08784e973543656c97d186a946d4799bbdec
thddlmy <thddlmy@gmail.com>
2021-10-10 21:42:52 +0900

    fix: 오타수정

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import store from '~/store'
 import router from '~/routes'
 import 'bootswatch/dist/litera/bootstrap.min.css'
 
-const { createApp } = Vue
+// const { createApp } = Vue
 const app = createApp(App)
 
 app.use(store)
